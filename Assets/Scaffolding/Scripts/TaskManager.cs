4c862d344c19883005c55b14eaff42b4c89a9f42
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2024-03-14 15:15:39 +0100

    Moving from laptop to desktop

--- Assets/Scaffolding/Scripts/TaskManager.cs
+++ Assets/Scaffolding/Scripts/TaskManager.cs
@@ -7,7 +7,7 @@ using Task;
 public class TaskManager : MonoBehaviour
 {
     private const string MAIN_TASK_NAME = "Bygg stillas";
-    private Dictionary<string, BuildObjective> _tasks = new Dictionary<string, BuildObjective>();
+    private Dictionary<string, BuildObjective> _objectives = new Dictionary<string, BuildObjective>();
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +16,8 @@ public class TaskManager : MonoBehaviour
         Task.Task tabletMainTask = tabletTaskHolder.GetTask(MAIN_TASK_NAME);
 
         // Tasks should be built in reverse order because they activate the next task when they are completed
-        BuildObjective secondTask = RegisterBuildObjective(tabletMainTask, "SubtaskName", null, "Tag", null);
-        BuildObjective firstTask = RegisterBuildObjective(tabletMainTask, "SubtaskName", "StepName", "Tag", secondTask);
+        BuildObjective secondTask = RegisterBuildObjective("Tag", "Tag", tabletMainTask, "SubtaskName", null, null);
+        BuildObjective firstTask = RegisterBuildObjective("Tag", "Tag", tabletMainTask, "SubtaskName", "StepName", secondTask);
     }
 
     // Update is called once per frame
@@ -26,7 +26,7 @@ public class TaskManager : MonoBehaviour
         
     }
 
-    private BuildObjective RegisterBuildObjective(Task.Task mainTask, string subtaskName, string? stepName, string partsTag, BuildObjective? next)
+    private BuildObjective RegisterBuildObjective(string loosePartsTag, string assembledPartsTag, Task.Task mainTask, string subtaskName, string? stepName, BuildObjective? next)
     {
         // We do not care if we are dealing with a task, subtask or step, so we cast to ICompletable
         ICompletable objective = stepName == null ?
@@ -35,12 +35,23 @@ public class TaskManager : MonoBehaviour
 
         // We find all GameObjects tagged with the given tag so they can be manipulated as a group
         List<GameObject> parts = new List<GameObject>();
-        parts.AddRange(GameObject.FindGameObjectsWithTag(partsTag));
+        parts.AddRange(GameObject.FindGameObjectsWithTag(assembledPartsTag));
 
+        // Provide the assembled part tag to the objective so the state can be manipulated correctly
         BuildObjective buildObjective = new BuildObjective(objective, parts, next);
-        _tasks[partsTag] = buildObjective;
+        _objectives[loosePartsTag] = buildObjective;
 
         return buildObjective;
+    }
+
+    public void RegisterPickup(string loosePartTag)
+    {
+        // If we want pickup to be a step of every objective we can use this method
+    }
+
+    public void RegisterPlacement(string loosePartTag)
+    {
+        _objectives[loosePartTag].Complete();
     }
 }
 
